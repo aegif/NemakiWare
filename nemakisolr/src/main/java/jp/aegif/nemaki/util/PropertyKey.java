bfeadbfc161418e317d5bbbe57034e72df277ad4
linzhixing <tomoyuki.hayashi@aegif.jp>
2014-03-18 22:03:35 +0100

    Arrange properties @NemakiSolr

---
+++ nemakisolr/src/main/java/jp/aegif/nemaki/util/PropertyKey.java
@@ -0,0 +1,23 @@
+package jp.aegif.nemaki.util;
+
+public interface PropertyKey {
+	public final String SOLR_TRACKING_CRON_ENABLED = "solr.tracking.cron.enabled";
+	public final String SOLR_TRACKING_CRON_EXPRESSION = "solr.tracking.cron.expression";
+	public final String SOLR_TRACKING_MIMETYPE = "solr.tracking.mimetype";
+	public final String SOLR_CORE_MAIN = "solr.core.main";
+	public final String SOLR_CORE_TOKEN = "solr.core.token";
+
+	public final String CMIS_SERVER_PROTOCOL = "cmis.server.protocol";
+	public final String CMIS_SERVER_HOST = "cmis.server.host";
+	public final String CMIS_SERVER_PORT = "cmis.server.port";
+	public final String CMIS_SERVER_CONTEXT = "cmis.server.context";
+	public final String CMIS_SERVER_WS_ENDPOINT = "cmis.server.ws.endpoint";
+	public final String CMIS_CHANGELOG_ITEMS = "cmis.changelog.items";
+	public final String CMIS_LOCALE_COUNTRY = "cmis.locale.country";
+	public final String CMIS_LOCALE_LANGUAGE = "cmis.locale.language";
+	public final String CMIS_REPOSITORY_MAIN = "repository.main";
+	public final String CMIS_PRINCIPAL_ADMIN_ID = "principal.admin.id";
+	public final String CMIS_PRINCIPAL_ADMIN_PASSWORD = "principal.admin.password";
+
+	public final String OVERRIDE_FILES = "override.files";
+}
