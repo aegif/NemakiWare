ea49cbfde309a806274474d39cc5cc3c7b301783
linzhixing <tomoyuki.hayashi@aegif.jp>
2014-03-27 21:23:29 +0100

    [@ware]Pass port value to CouchConnector

--- nemakiware/src/main/java/jp/aegif/nemaki/service/db/CouchConnector.java
+++ nemakiware/src/main/java/jp/aegif/nemaki/service/db/CouchConnector.java
@@ -1,21 +1,21 @@
 /*******************************************************************************
  * Copyright (c) 2013 aegif.
- * 
+ *
  * This file is part of NemakiWare.
- * 
+ *
  * NemakiWare is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * NemakiWare is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with NemakiWare.
  * If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Contributors:
  *     linzhixing(https://github.com/linzhixing) - initial API and implementation
  ******************************************************************************/
@@ -51,23 +51,27 @@ public class CouchConnector {
 	private String[] repositoryId;
 
 	/**
+	 * Port, for instance: 5984
+	 */
+	private int port;
+
+	/**
 	 * Max connections, for instance: 40
 	 */
 	private int maxConnections;
-
 
 	/**
 	 * Initialize this class with host, maxConnections.
 	 */
 	public void init() {
 		HttpClient httpClient = new StdHttpClient.Builder().host(host)
-				.maxConnections(maxConnections).build();
+				.port(port).maxConnections(maxConnections).build();
 		CouchDbInstance dbInstance = new StdCouchDbInstance(httpClient);
 
-		String repo = "";		
-		try{
+		String repo = "";
+		try {
 			repo = repositoryId[0];
-		}catch(Exception ex){
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 		connector = dbInstance.createConnector(repo, true);
@@ -77,12 +81,16 @@ public class CouchConnector {
 		return connector;
 	}
 
-	public void setRepositoryId(String[] repositoryId) {	
+	public void setRepositoryId(String[] repositoryId) {
 		this.repositoryId = repositoryId;
 	}
-	
+
 	public void setHost(String host) {
 		this.host = host;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
 	}
 
 	public void setMaxConnections(int maxConnections) {
