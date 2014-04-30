5115c9f0601f2d340acda502475a4b8da63488ac
linzhixing <tomoyuki.hayashi@aegif.jp>
2014-04-30 14:00:31 +0200

    [solr]Modify the mode string "AUTO" to "DELTA"

--- nemakisolr/src/main/java/jp/aegif/nemaki/tracker/CoreTrackerJob.java
+++ nemakisolr/src/main/java/jp/aegif/nemaki/tracker/CoreTrackerJob.java
@@ -1,25 +1,27 @@
 /*******************************************************************************
  * Copyright (c) 2013 aegif.
- * 
+ *
  * This file is part of NemakiWare.
- * 
+ *
  * NemakiWare is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * NemakiWare is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with NemakiWare.
  * If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Contributors:
  *     linzhixing(https://github.com/linzhixing) - initial API and implementation
  ******************************************************************************/
 package jp.aegif.nemaki.tracker;
+
+import jp.aegif.nemaki.util.Constant;
 
 import org.apache.log4j.Logger;
 import org.quartz.Job;
@@ -32,29 +34,28 @@ import org.quartz.JobExecutionException;
  *
  */
 public class CoreTrackerJob implements Job{
-	
-	private final String MODE_AUTO = "AUTO";
-	
+
 	Logger logger = Logger.getLogger(CoreTrackerJob.class);
-	
+
 	public CoreTrackerJob(){
 		super();
 	}
-	
+
+	@Override
 	public void execute(JobExecutionContext jec) throws JobExecutionException{
 		CoreTracker coreTracker = (CoreTracker) jec.getJobDetail().getJobDataMap().get("TRACKER");
-		
+
 		if ( !coreTracker.isConnectionSetup() ) {
 			coreTracker.setupCmisSession();
 		}
-		
+
 		if(coreTracker.cmisSession == null){
 			coreTracker.setupCmisSession();
 		}
 		if(coreTracker.cmisSession == null){
 			logger.error("Tracking is not executed because the session to the CMIS server is not established.");
 		}else{
-			coreTracker.index(MODE_AUTO);
+			coreTracker.index(Constant.MODE_DELTA);
 		}
 	}
 }
---
+++ nemakisolr/src/main/java/jp/aegif/nemaki/util/Constant.java
@@ -0,0 +1,6 @@
+package jp.aegif.nemaki.util;
+
+public interface Constant {
+	String MODE_FULL = "FULL";
+	String MODE_DELTA ="DELTA";
+}
