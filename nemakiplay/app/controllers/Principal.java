764f69173d874b237dd7061b7f83617eb1dac17d
linzhixing <tomoyuki.hayashi@aegif.jp>
2014-10-27 13:50:54 +0900

    First commit of new UI(Play Framework)

---
+++ nemakiplay/app/controllers/Principal.java
@@ -0,0 +1,68 @@
+package controllers;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.mvc.Security.Authenticated;
+import util.Util;
+
+@Authenticated(Secured.class)
+public class Principal extends Controller{
+	public static Result search(String term){
+		List<model.Principal>principals = new ArrayList<>();
+		
+		//user search
+		JsonNode resultUsers = Util.getJsonResponse("http://localhost:8080/nemakiware/rest/user/search?query=" + term);
+    	//TODO check status
+    	JsonNode users = resultUsers.get("result");
+		if(users != null){
+			Iterator<JsonNode> userItr = users.iterator();
+			while(userItr.hasNext()){
+				JsonNode user = userItr.next();
+				
+				model.Principal p = new model.Principal("user", user.get("userId").asText(), user.get("userName").asText());
+				principals.add(p);
+			}
+		}
+		
+		//group search
+		JsonNode resultGroups = Util.getJsonResponse("http://localhost:8080/nemakiware/rest/group/search?query=" + term);
+    	//TODO check status
+    	JsonNode groups = resultGroups.get("result");
+		if(groups != null){
+			Iterator<JsonNode> groupItr = groups.iterator();
+			while(groupItr.hasNext()){
+				JsonNode group = groupItr.next();
+				
+				model.Principal p = new model.Principal("group", group.get("groupId").asText(), group.get("groupName").asText());
+				principals.add(p);
+			}
+		}
+    	
+		//convert
+		 final ByteArrayOutputStream out = new ByteArrayOutputStream();
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			mapper.writeValue(out, principals);
+			 final byte[] data = out.toByteArray();
+			 JsonNode converted = Json.parse(new String(data));
+			 
+			 return ok(converted);
+			 
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return ok();
+	}
+}
---
+++ nemakiplay/app/model/Principal.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Principal {
+	public String genre;
+	public String id;
+	public String name;
+	
+	
+	public Principal(){
+		
+	}
+	
+	public Principal(String genre, String id, String name){
+		this.genre = genre;
+		this.id = id;
+		this.name = name;
+	}
+}
