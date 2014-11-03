58100ef7cb7a45f9ec9455fd29f7a86e37bd7f57
linzhixing <tomoyuki.hayashi@aegif.jp>
2014-11-04 02:13:03 +0900

    Implement UI property overwrite system

--- ui/app/constant/PropertyKey.java
+++ ui/app/constant/PropertyKey.java
@@ -3,4 +3,9 @@ package constant;
 public interface PropertyKey {
 	final String PROPERTY_FILES = "property.files";
 	final String NEMAKI_CORE_URI= "nemaki.core.uri";
+	final String NEMAKI_CORE_URI_PROTOCOL= "nemaki.core.uri.protocol";
+	final String NEMAKI_CORE_URI_HOST= "nemaki.core.uri.host";
+	final String NEMAKI_CORE_URI_PORT= "nemaki.core.uri.port";
+	final String NEMAKI_CORE_URI_CONTEXT= "nemaki.core.uri.context";
+	final String NEMAKI_CORE_URI_REPOSITORY= "nemaki.core.uri.repository";
 }
