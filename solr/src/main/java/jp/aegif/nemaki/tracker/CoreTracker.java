903405e86345096b82b6eacd2bae1403c2b4cfe8
linzhixing <tomoyuki.hayashi@aegif.jp>
2015-08-22 02:36:58 +0900

    Extract CmisSessionFactory from Solr CoreTracker class so that NemakiCoreAdminHandler can use it.
    Former-commit-id: e7e9627a7a121cd25d6c1db790361065fc4c7723

--- solr/src/main/java/jp/aegif/nemaki/tracker/CoreTracker.java
+++ solr/src/main/java/jp/aegif/nemaki/tracker/CoreTracker.java
@@ -37,6 +37,7 @@ import java.util.Map;
 import java.util.Set;
 
 import jp.aegif.nemaki.NemakiCoreAdminHandler;
+import jp.aegif.nemaki.util.CmisSessionFactory;
 import jp.aegif.nemaki.util.Constant;
 import jp.aegif.nemaki.util.PropertyKey;
 import jp.aegif.nemaki.util.PropertyManager;
@@ -82,7 +83,7 @@ public class CoreTracker extends CloseHook {
 	SolrCore core;
 	SolrServer repositoryServer;
 	SolrServer tokenServer;
-	Session cmisSession;
+	
 	CmisBinding cmisBinding;
 	NemakiTokenManager nemakiTokenManager;
 
@@ -97,99 +98,7 @@ public class CoreTracker extends CloseHook {
 		this.nemakiTokenManager = new NemakiTokenManager();
 	}
 	
-	public void setupCmisSession() {
-		PropertyManager pm = new PropertyManagerImpl(StringPool.PROPERTIES_NAME);
-
-		String protocol = pm.readValue(PropertyKey.CMIS_SERVER_PROTOCOL);
-		String host = pm.readValue(PropertyKey.CMIS_SERVER_HOST);
-		String port = pm.readValue(PropertyKey.CMIS_SERVER_PORT);
-		String context = pm.readValue(PropertyKey.CMIS_SERVER_CONTEXT);
-		String wsEndpoint = pm.readValue(PropertyKey.CMIS_SERVER_WS_ENDPOINT);
-		String url = getCmisUrl(protocol, host, port, context, wsEndpoint);
-
-		String repository = pm.readValue(PropertyKey.CMIS_REPOSITORY_MAIN);
-		String country = pm.readValue(PropertyKey.CMIS_LOCALE_COUNTRY);
-		String language = pm.readValue(PropertyKey.CMIS_LOCALE_LANGUAGE);
-		String user = pm.readValue(PropertyKey.CMIS_PRINCIPAL_ADMIN_ID);
-		String password = pm
-				.readValue(PropertyKey.CMIS_PRINCIPAL_ADMIN_PASSWORD);
-
-		SessionFactory f = SessionFactoryImpl.newInstance();
-		Map<String, String> parameter = new HashMap<String, String>();
-
-		// user credentials
-		parameter.put(SessionParameter.USER, user);
-		parameter.put(SessionParameter.PASSWORD, password);
-
-		// session locale
-		parameter.put(SessionParameter.LOCALE_ISO3166_COUNTRY, country);
-		parameter.put(SessionParameter.LOCALE_ISO639_LANGUAGE, language);
-
-		// repository
-		parameter.put(SessionParameter.REPOSITORY_ID, repository);
-
-		// WebServices ports
-		parameter.put(SessionParameter.BINDING_TYPE,
-				BindingType.WEBSERVICES.value());
-
-		parameter.put(SessionParameter.WEBSERVICES_ACL_SERVICE, url
-				+ "ACLService?wsdl");
-		parameter.put(SessionParameter.WEBSERVICES_DISCOVERY_SERVICE, url
-				+ "DiscoveryService?wsdl");
-		parameter.put(SessionParameter.WEBSERVICES_MULTIFILING_SERVICE, url
-				+ "MultiFilingService?wsdl");
-		parameter.put(SessionParameter.WEBSERVICES_NAVIGATION_SERVICE, url
-				+ "NavigationService?wsdl");
-		parameter.put(SessionParameter.WEBSERVICES_OBJECT_SERVICE, url
-				+ "ObjectService?wsdl");
-		parameter.put(SessionParameter.WEBSERVICES_POLICY_SERVICE, url
-				+ "PolicyService?wsdl");
-		parameter.put(SessionParameter.WEBSERVICES_RELATIONSHIP_SERVICE, url
-				+ "RelationshipService?wsdl");
-		parameter.put(SessionParameter.WEBSERVICES_REPOSITORY_SERVICE, url
-				+ "RepositoryService?wsdl");
-		parameter.put(SessionParameter.WEBSERVICES_VERSIONING_SERVICE, url
-				+ "VersioningService?wsdl");
-		
-			//Auth token
-			Boolean authTokenEnabled = 
-					Boolean.valueOf(pm.readValue(PropertyKey.NEMAKI_CAPABILITY_EXTENDED_AUTH_TOKEN));
-			if(authTokenEnabled){
-				parameter.put(SessionParameter.AUTHENTICATION_PROVIDER_CLASS, "jp.aegif.nemaki.util.NemakiAuthenticationProvider");
-				String authToken = nemakiTokenManager.getOrRegister(user, password);
-				parameter.put(Constant.AUTH_TOKEN, authToken);
-				parameter.put(Constant.AUTH_TOKEN_APP, "ui");
-			}
-		
-		try {
-			// create session
-			cmisSession = f.createSession(parameter);
-			OperationContext operationContext = cmisSession
-					.createOperationContext(null, false, false, false, null,
-							null, false, null, true, 100);
-			cmisSession.setDefaultContext(operationContext);
-		} catch (Exception e) {
-			logger.error("Failed to create a session to CMIS server", e);
-		}
-	}
-
-	private String getCmisUrl(String protocol, String host, String port,
-			String context, String wsEndpoint) {
-		try {
-			URL url = new URL(protocol, host, Integer.parseInt(port), "");
-			return url.toString() + "/" + context + "/" + wsEndpoint + "/";
-		} catch (NumberFormatException e) {
-			logger.error("", e);
-			e.printStackTrace();
-		} catch (MalformedURLException e) {
-			logger.error("", e);
-		}
-		return null;
-	}
-
-	public boolean isConnectionSetup() {
-		return (this.cmisSession != null);
-	}
+	
 
 	@Override
 	public void preClose(SolrCore core) {
@@ -204,10 +113,6 @@ public class CoreTracker extends CloseHook {
 	 */
 	public void initCore() {
 		synchronized (LOCK) {
-			if(!isConnectionSetup()){
-				setupCmisSession();
-			}
-			
 			try {
 				// Initialize all documents
 				repositoryServer.deleteByQuery("*:*");
@@ -231,10 +136,6 @@ public class CoreTracker extends CloseHook {
 	 */
 	public void index(String trackingType) {
 		synchronized (LOCK) {
-			if(!isConnectionSetup()){
-				setupCmisSession();
-			}
-			
 			ChangeEvents changeEvents = getCmisChangeLog(trackingType);
 			List<ChangeEvent> events = changeEvents.getChangeEvents();
 
@@ -288,6 +189,7 @@ public class CoreTracker extends CloseHook {
 
 				List<ChangeEvent> listPerThread = list.subList(numberPerThread
 						* i, toIndex);
+				Session cmisSession = CmisSessionFactory.getSession();
 				Registration registration = new Registration(cmisSession, core,
 						repositoryServer, listPerThread, allowedMimeTypeFilter);
 				Thread t = new Thread(registration);
@@ -385,6 +287,7 @@ public class CoreTracker extends CloseHook {
 		long numItems = (-1 == _numItems) ? Long.MAX_VALUE : Long
 				.valueOf(_numItems);
 
+		Session cmisSession = CmisSessionFactory.getSession();
 		ChangeEvents changeEvents = cmisSession.getContentChanges(latestToken,false, numItems);
 
 		// No need for Sorting
