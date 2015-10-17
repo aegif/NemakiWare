ab130035366e17250c45bc17ded52b294ea03784
linzhixing <tomoyuki.hayashi@aegif.jp>
2015-10-17 14:38:08 +0900

    Fix #162 Add userId key to the pool of CMIS sessions
    From (repositoryId, session) to (repositoryId, (userId, session))
    
    Former-commit-id: e88aaedb317281614d2c0e9a2c766c15e336a818

--- ui/app/controllers/CmisSessions.java
+++ ui/app/controllers/CmisSessions.java
@@ -5,17 +5,36 @@ import java.util.Map;
 
 import org.apache.chemistry.opencmis.client.api.Session;
 
+import constant.Token;
 import util.Util;
 
 public class CmisSessions {
-	private static Map<String, Session> cmisSessions = new HashMap<String, Session>();
+	private static Map<String, CmisSessionsOfRepository> cmisSessions = new HashMap<String, CmisSessionsOfRepository>();
+	
+	static class CmisSessionsOfRepository{
+		private Map<String, Session> map = new HashMap<String, Session>();
+		public Session get(String userId){
+			return map.get(userId);
+		}
+		public void put(String userId, Session session){
+			map.put(userId, session);
+		}
+	}
 	
 	public static Session getCmisSession(String repositoryId, play.mvc.Http.Session session){
-		Session cmisSession = cmisSessions.get(repositoryId);
+		CmisSessionsOfRepository repoSession = cmisSessions.get(repositoryId);
+		if(repoSession == null){
+			repoSession = new CmisSessionsOfRepository();
+			cmisSessions.put(repositoryId, repoSession);
+		}
+		
+		String userId = session.get(Token.LOGIN_USER_ID);
+		Session cmisSession = repoSession.get(userId);
 		if(cmisSession == null){
 			cmisSession = Util.createCmisSession(repositoryId, session);
-			cmisSessions.put(repositoryId, cmisSession);
+			repoSession.put(userId, cmisSession);
 		}
+		
 		return cmisSession;
 	}
 }
