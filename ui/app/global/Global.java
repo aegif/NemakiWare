3946d353e9ade9b60f05d8e27b15351875c1bd02
git -v <takuma.sugimoto@aegif.jp>
2015-12-04 18:14:58 +0900

    Modify : Filter -> Global hook
    Former-commit-id: 0acf2dbbf28fb38de695cd5d91ee8160d798f074

--- ui/app/global/Global.java
+++ ui/app/global/Global.java
@@ -2,13 +2,39 @@ package global;
 import java.util.HashMap;
 import java.util.Map;
 
-import filters.NoCacheFilter;
 import play.Application;
 import play.GlobalSettings;
 import play.Logger;
-import play.api.mvc.EssentialFilter;
+import play.libs.F.Promise;
+import play.mvc.Action;
+import play.mvc.Http;
+import play.mvc.Result;
 
 public class Global extends GlobalSettings {
+
+    private class ActionWrapper extends Action.Simple {
+        public ActionWrapper(Action<?> action) {
+            this.delegate = action;
+        }
+
+        @Override
+        public Promise<Result> call(Http.Context ctx) throws java.lang.Throwable {
+            Promise<Result> result = this.delegate.call(ctx);
+            Http.Response response = ctx.response();
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Cache-Control",  "no-cache, no-store, must-revalidate, max-age=0, post-check=0, pre-check=0"); // HTTP 1.1
+            response.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+            response.setHeader("EXPIRES", "0"); // Proxies
+            return result;
+        }
+    }
+
+    @Override
+    public Action<?> onRequest(Http.Request request, java.lang.reflect.Method actionMethod) {
+        return new ActionWrapper(super.onRequest(request, actionMethod));
+    }
+
+
 
 	private Map<String, String>test = new HashMap<String, String>();
 
@@ -21,12 +47,6 @@ public class Global extends GlobalSettings {
 	public void onStop(Application arg0) {
 		Logger.info("Application shutdown...");
 	}
-
-    @SuppressWarnings("unchecked")
-	@Override
-    public <T extends EssentialFilter> Class<T>[] filters() {
-        return new Class[] {NoCacheFilter.class};
-    }
 
 
 	public String getValue(String key){
