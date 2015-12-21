7afa0f4914fa87558c5297987d4ff212ca634f9c
git -v <takuma.sugimoto@aegif.jp>
2015-12-21 22:08:21 +0900

    Modify : show product version
    Former-commit-id: 161e3dff31aafc9deb145ba2da4829c07fe6f726

--- ui/app/constant/Token.java
+++ ui/app/constant/Token.java
@@ -4,12 +4,14 @@ public interface Token {
 	final String LOGIN_USER_ID = "loginUserId";
 	final String LOGIN_USER_PASSWORD = "loginUserPassword";
 	final String LOGIN_USER_IS_ADMIN = "loginUserIsAdmin";
-	
+
+	final String NEMAKIWARE_VERSION = "NemakiWareProductVersion";
+
 	final String REST_STATUS = "status";
 	final String REST_SUCCESS = "success";
 	final String REST_RESULT = "result";
 	final String REST_ERROR = "error";
-	
+
 	final String PRINCIPAL_GENRE_USER = "user";
 	final String PRINCIPAL_GENRE_GROUP = "group";
 }
