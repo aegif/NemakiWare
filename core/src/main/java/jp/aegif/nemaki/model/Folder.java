72197881e02eb4ada1e62afc20c7469a0ddf1019
git -v <takuma.sugimoto@aegif.jp>
2015-12-21 18:45:55 +0900

    Fix : Fix getFolderByPath method.
    Former-commit-id: 29c6c1f094effe9ae9621453cff8b89046393187

--- core/src/main/java/jp/aegif/nemaki/model/Folder.java
+++ core/src/main/java/jp/aegif/nemaki/model/Folder.java
@@ -1,21 +1,21 @@
 /*******************************************************************************
  * Copyright (c) 2013 aegif.
- * 
+ *
  * This file is part of NemakiWare.
- * 
+ *
  * NemakiWare is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * NemakiWare is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with NemakiWare.
  * If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Contributors:
  *     linzhixing(https://github.com/linzhixing) - initial API and implementation
  ******************************************************************************/
@@ -38,13 +38,13 @@ public class Folder extends Content {
 
 	private List<String> allowedChildTypeIds;
 	private List<String> renditionIds;
-	
-	
+
+
 	public Folder(){
 		super();
 		setType(NodeType.CMIS_FOLDER.value());
 	}
-	
+
 	public Folder(Content c){
 		super(c);
 		setName(c.getName());
@@ -58,7 +58,7 @@ public class Folder extends Content {
 		setObjectType(c.getObjectType());
 		setChangeToken(c.getChangeToken());
 	}
-	
+
 	public List<String> getAllowedChildTypeIds() {
 		return allowedChildTypeIds;
 	}
