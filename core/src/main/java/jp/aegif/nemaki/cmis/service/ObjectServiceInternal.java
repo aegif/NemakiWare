7d4c9e6d9eedd80a5ed0c050c4826e1fafd8779c
linzhixing <tomoyuki.hayashi@aegif.jp>
2016-01-22 00:40:26 +0900

    Modify deleteTree to use multi threads for improving performance

--- core/src/main/java/jp/aegif/nemaki/cmis/service/ObjectServiceInternal.java
+++ core/src/main/java/jp/aegif/nemaki/cmis/service/ObjectServiceInternal.java
@@ -2,7 +2,20 @@ package jp.aegif.nemaki.cmis.service;
 
 import org.apache.chemistry.opencmis.commons.server.CallContext;
 
+import jp.aegif.nemaki.model.Content;
+
 public interface ObjectServiceInternal {
+	/**
+	 * wrap method
+	 * @param callContext
+	 * @param repositoryId
+	 * @param content
+	 * @param allVersions
+	 * @param deleteWithParent
+	 */
+	public abstract void deleteObjectInternal(CallContext callContext, String repositoryId,
+			Content content, Boolean allVersions, Boolean deleteWithParent);
+	
 	/**
 	 * Delete each object together with deleteWithParent flag archived
 	 * @param callContext
