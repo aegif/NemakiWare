74635b4f3e1d21cca62efe1906bec0b50993349a
git -v <takuma.sugimoto@aegif.jp>
2016-03-04 17:27:15 +0900

    Modify : Add args
    Former-commit-id: 391844b08c9cd18509d0a888e7eb9418a1cc4b86

--- setup/aws/tsuridoko/src/main/java/jp/aegif/nemaki/aws/tools/backup/EntryPoint.java
+++ setup/aws/tsuridoko/src/main/java/jp/aegif/nemaki/aws/tools/backup/EntryPoint.java
@@ -1,10 +1,37 @@
 package jp.aegif.nemaki.aws.tools.backup;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 public class EntryPoint {
 
 	public static void main(String[] args) {
-		BackupCouchDbToS3Util util = new BackupCouchDbToS3Util();
-		util.backup();
+
+		// create Options object
+		Options options = new Options();
+
+		// add t option
+		options.addOption("b", true, "Backup S3 bucket name.");
+
+	    try {
+	    	CommandLineParser parser = new DefaultParser();
+	        CommandLine cmd = parser.parse( options, args );
+			if(cmd.hasOption("b")) {
+				String bucketName = cmd.getOptionValue("b");
+				BackupCouchDbToS3Util util = new BackupCouchDbToS3Util();
+				util.backup(bucketName);
+			}
+	    }
+	    catch( ParseException exp ) {
+	        System.err.println( "Parsing failed.  Reason: " + exp.getMessage() );
+	    }
+
+
+
+
 
 	}
 
