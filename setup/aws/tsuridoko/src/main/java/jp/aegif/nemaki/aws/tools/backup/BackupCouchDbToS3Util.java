1eae57e5880aa9f31c821200bf6bf45febe8c457
git -v <takuma.sugimoto@aegif.jp>
2016-03-07 21:16:36 +0900

    Modify :  get repositories from rest local url

--- setup/aws/tsuridoko/src/main/java/jp/aegif/nemaki/aws/tools/backup/BackupCouchDbToS3Util.java
+++ setup/aws/tsuridoko/src/main/java/jp/aegif/nemaki/aws/tools/backup/BackupCouchDbToS3Util.java
@@ -24,21 +24,22 @@ import javax.ws.rs.client.*;
 import javax.ws.rs.core.*;
 
 public class BackupCouchDbToS3Util {
-	static String DefaultCouchDbUrl  = "http://localhost:5984/";
+	static String DefaultCouchDbUrl = "http://localhost:5984/";
 	static String DefaultProfileName = "NemakiWare";
 
-	public void backup(String bucketName)  {
-		try{
+	public void backup(String bucketName) {
+		try {
 			URI uri = new URI(DefaultCouchDbUrl);
-		backup(bucketName, uri, DefaultProfileName,  new String[0]);
-		}catch(Exception ex){
+			backup(bucketName, uri, DefaultProfileName, new String[0]);
+		} catch (Exception ex) {
 		}
 	}
-	public void backup(String bucketName, URI url)  {
+
+	public void backup(String bucketName, URI url) {
 		backup(bucketName, url, DefaultProfileName, new String[0]);
 	}
 
-	public void backup(String bucketName, URI url, String profileName)  {
+	public void backup(String bucketName, URI url, String profileName) {
 		backup(bucketName, url, profileName, new String[0]);
 	}
 
@@ -46,29 +47,35 @@ public class BackupCouchDbToS3Util {
 		backup(bucketName, url, profileName, targets.split(","));
 	}
 
-	public void backup(String bucketName, URI couchUrl, String profileName, String[] targets)  {
+	public void backup(String bucketName, URI couchUrl, String profileName, String[] targets) {
 		String[] targetDbs = targets;
 		AmazonS3 s3client = new AmazonS3Client(new ProfileCredentialsProvider(profileName));
+		String uriScheme = couchUrl.getScheme();
 
 		if (targetDbs == null || targetDbs.length == 0) {
-			List<String> list = getAllTargets(couchUrl);
+			URI targetRestURL = couchUrl;
+			if (uriScheme.equals("file")) {
+				try {
+					targetRestURL = new URI(DefaultCouchDbUrl);
+				} catch (URISyntaxException e) {
+				}
+			}
+			List<String> list = getAllTargets(targetRestURL);
 			targetDbs = (String[]) list.toArray(new String[0]);
 		}
-		String uriScheme = couchUrl.getScheme();
 
 		for (String repositoryName : targetDbs) {
 			File file;
 			try {
-
-				if(uriScheme.equals("http") || uriScheme.equals("https")){
-								file = File.createTempFile(repositoryName, ".bk.dump");
-								String couchURI = couchUrl.toString();
-								DumpAction action = DumpAction.getInstance(couchURI, repositoryName, file, false);
-								action.dump();
-				}else if(uriScheme.equals("file")){
+				if (uriScheme.equals("http") || uriScheme.equals("https")) {
+					file = File.createTempFile(repositoryName, ".bk.dump");
+					String couchURI = couchUrl.toString();
+					DumpAction action = DumpAction.getInstance(couchURI, repositoryName, file, false);
+					action.dump();
+				} else if (uriScheme.equals("file")) {
 					couchUrl = couchUrl.resolve(repositoryName + ".couch");
 					file = new File(couchUrl);
-				}else{
+				} else {
 					System.out.println("Error : Invalid scheme.");
 					continue;
 				}
@@ -84,6 +91,7 @@ public class BackupCouchDbToS3Util {
 			}
 		}
 	}
+
 	public List<String> getAllTargets(URI couchUrl) {
 		Client client = ClientBuilder.newClient();
 		String jsonArrayString = client.target(couchUrl).path("_all_dbs").request(MediaType.APPLICATION_JSON_TYPE)
@@ -95,6 +103,7 @@ public class BackupCouchDbToS3Util {
 		}
 		return list;
 	}
+
 	public void uploadS3(AmazonS3 s3client, File file, String bucketName, String repositoryName) {
 		try {
 			s3client.putObject(new PutObjectRequest(bucketName, repositoryName, file));
