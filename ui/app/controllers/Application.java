8e6aa531e8202c4377c45e6b8399541d2dffac01
linzhixing <tomoyuki.hayashi@aegif.jp>
2016-03-19 13:49:31 +0900

    Implement restore archive function in view
    Former-commit-id: 3c0d7c2fb31b2e659432b03d803f37c78a66bf52

--- ui/app/controllers/Application.java
+++ ui/app/controllers/Application.java
@@ -85,6 +85,9 @@ public class Application extends Controller{
 				controllers.routes.javascript.Node.getAce(),
 				controllers.routes.javascript.Node.update(),
 				controllers.routes.javascript.Node.delete(),
+				
+				controllers.routes.javascript.Archive.index(),
+				controllers.routes.javascript.Archive.restore(),
 
 				controllers.routes.javascript.Type.showBlank(),
 				controllers.routes.javascript.Type.edit(),
