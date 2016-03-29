820e134e69cddc58bebb1fe7306c2c52540b27d9
linzhixing <tomoyuki.hayashi@aegif.jp>
2016-03-29 16:45:52 +0900

    Add document/folder icon in archive page
    Former-commit-id: f84420c86b060994e6b6cd91cd93c04df9a7f24e

--- ui/app/model/Archive.java
+++ ui/app/model/Archive.java
@@ -11,28 +11,32 @@ public class Archive {
 	public String originalId;
 	public String parentId;
 	public String created;
-	public String type;
+	public String baseType;
 	public String creator;
 	
 	public Archive(ObjectNode json){
 		
 		JsonNode id = json.get("id");
 		if(id != null) this.id = id.textValue();
+		
+		JsonNode originalId = json.get("originalId");
+		if(originalId != null) this.originalId = originalId.textValue();
 	
 		JsonNode parentId = json.get("parentId");
 		if(parentId != null) this.parentId = parentId.textValue();
 	
-		JsonNode created = json.get("created");
-		if(created != null) this.created = created.textValue();
-	
 		JsonNode name = json.get("name");
 		if(name != null) this.name = name.textValue();
 	
-		JsonNode type = json.get("type");
-		if(type != null) this.type = type.textValue();
+		JsonNode baseType = json.get("type");
+		if(baseType != null) this.baseType = baseType.textValue();
 	
 		JsonNode creator = json.get("creator");
 		if(creator != null) this.creator = creator.textValue();
+		
+		JsonNode created = json.get("created");
+		if(created != null) this.created = created.textValue();
+	
 	}
 		
 }
