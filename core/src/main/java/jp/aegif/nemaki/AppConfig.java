25d722cb0a3cd78d062a17ca70d59e49e75f5640
SUGIMOTO Takuma <takuma.sugimoto@aegif.jp>
2016-08-24 14:38:42 +0900

    Add core plugin

--- core/src/main/java/jp/aegif/nemaki/AppConfig.java
+++ core/src/main/java/jp/aegif/nemaki/AppConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.*;
 
 import jp.aegif.nemaki.plugin.action.JavaBackedAction;
 import jp.aegif.nemaki.util.action.NemakiActionPlugin;
+import org.springframework.plugin.core.config.EnablePluginRegistries;
 
+@EnablePluginRegistries(JavaBackedAction.class)
 @ImportResource("classpath*:META-INF/plugins.xml")
 @Configuration
 public class AppConfig {
