40eac69686254223f9510703545a465a481d16d7
SUGIMOTO Takuma <takuma.sugimoto@aegif.jp>
2016-09-20 18:05:42 +0900

    Sample update

--- core/src/main/java/jp/aegif/nemaki/util/action/NemakiActionPlugin.java
+++ core/src/main/java/jp/aegif/nemaki/util/action/NemakiActionPlugin.java
@@ -2,6 +2,7 @@ package jp.aegif.nemaki.util.action;
 
 import org.apache.chemistry.opencmis.commons.data.ObjectData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ImportResource;
 
 import jp.aegif.nemaki.plugin.action.JavaBackedAction;
 
@@ -10,8 +11,16 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
 
+import org.springframework.plugin.core.PluginRegistry;
+
+
+@ImportResource("classpath*:META-INF/plugins.xml")
 public class NemakiActionPlugin {
+	@Autowired(required=false)
+	PluginRegistry<JavaBackedAction, String> pluginRegistry;
 
 	@Autowired(required=false)
 	Map<String, JavaBackedAction> pluginMap = new HashMap<String, JavaBackedAction>();
