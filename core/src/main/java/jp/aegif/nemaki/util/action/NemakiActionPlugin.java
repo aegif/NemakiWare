558a639051f472e193933a9e80d9576677f6589e
SUGIMOTO Takuma <takuma.sugimoto@aegif.jp>
2016-09-21 20:08:06 +0900

    Action return value

--- core/src/main/java/jp/aegif/nemaki/util/action/NemakiActionPlugin.java
+++ core/src/main/java/jp/aegif/nemaki/util/action/NemakiActionPlugin.java
@@ -1,20 +1,13 @@
 package jp.aegif.nemaki.util.action;
 
-import org.apache.chemistry.opencmis.commons.data.ObjectData;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.plugin.core.PluginRegistry;
 
 import jp.aegif.nemaki.plugin.action.JavaBackedAction;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.function.UnaryOperator;
-import java.util.stream.Collectors;
-
-import org.springframework.plugin.core.PluginRegistry;
 
 
 @ImportResource("classpath*:META-INF/plugins.xml")
