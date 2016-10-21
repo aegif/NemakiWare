85bf33e37de660047d26f355b0e19dea74b7b8d2
SUGIMOTO Takuma <takuma.sugimoto@aegif.jp>
2016-10-21 14:24:08 +0900

    Add config view

--- core/src/main/java/jp/aegif/nemaki/rest/ConfigResource.java
+++ core/src/main/java/jp/aegif/nemaki/rest/ConfigResource.java
@@ -1,9 +1,11 @@
 package jp.aegif.nemaki.rest;
 
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.locks.Lock;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -12,23 +14,76 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.commons.lang.StringUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.springframework.stereotype.Component;
 
+
+
+import jp.aegif.nemaki.common.ErrorCode;
 import jp.aegif.nemaki.dao.ContentDaoService;
 import jp.aegif.nemaki.model.Configuration;
+import jp.aegif.nemaki.model.User;
+import jp.aegif.nemaki.util.PropertyManager;
 import jp.aegif.nemaki.util.lock.ThreadLockService;
 
-@Path("/repo/{repositoryId}/config/")
+@Path("/repo/{repositoryId}/config")
 public class ConfigResource extends ResourceBase{
 	private ContentDaoService contentDaoService;
 	private ThreadLockService threadLockService;
+	private PropertyManager propertyManager;
+
+	@SuppressWarnings("unchecked")
+	@GET
+	@Path("/list")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String list(@PathParam("repositoryId") String repositoryId,
+			@Context HttpServletRequest httpRequest) {
+
+		boolean status = true;
+		JSONObject result = new JSONObject();
+		JSONObject config = new JSONObject();
+		JSONArray errMsg = new JSONArray();
+
+		try {
+			Set<String> keys = propertyManager.getKeys();
+			for(String configKey : keys){
+			Object value = propertyManager.readValue(repositoryId, configKey);
+			config.put(configKey, value);
+			}
+			result.put("configurations", config);
+		} catch (Exception e) {
+			status = false;
+			e.printStackTrace();
+			addErrMsg(errMsg, ITEM_ERROR, ErrorCode.ERR_LIST);
+		}
+
+		result = makeResult(status, result, errMsg);
+		return result.toJSONString();
+	}
+
+	@SuppressWarnings("unchecked")
+	@GET
+	@Path("/show/{key}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String show(@PathParam("repositoryId") String repositoryId, @PathParam("key") String configKey) {
+		boolean status = true;
+		JSONObject result = new JSONObject();
+		JSONArray errMsg = new JSONArray();
+
+		Object value = propertyManager.readValue(repositoryId, configKey);
+
+		result.put("configuration", value);
+		result = makeResult(status, result, errMsg);
+		return result.toJSONString();
+	}
+
 
 	@PUT
 	@Produces(MediaType.APPLICATION_JSON)
-	public String update(@PathParam("repositoryId") String repositoryId, 
-			@QueryParam("key") String key, @QueryParam("value") String value, 
+	public String update(@PathParam("repositoryId") String repositoryId,
+			@QueryParam("key") String key, @QueryParam("value") String value,
 			@Context HttpServletRequest httpRequest) {
 		boolean status = true;
 		JSONObject result = new JSONObject();
@@ -45,16 +100,20 @@ public class ConfigResource extends ResourceBase{
 		}finally{
 			lock.unlock();
 		}
-		
+
 		result = makeResult(status, result, errMsg);
 		return result.toJSONString();
 	}
-	
+
 	public void setContentDaoService(ContentDaoService contentDaoService) {
 		this.contentDaoService = contentDaoService;
 	}
 
 	public void setThreadLockService(ThreadLockService threadLockService) {
 		this.threadLockService = threadLockService;
+	}
+
+	public void setPropertyManager(PropertyManager propertyManager) {
+		this.propertyManager = propertyManager;
 	}
 }
---
+++ ui/app/controllers/Config.java
@@ -0,0 +1,73 @@
+package controllers;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.chemistry.opencmis.client.api.Session;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.mvc.Security.Authenticated;
+import util.Util;
+
+import views.html.config.*;
+
+@Authenticated(Secured.class)
+public class Config extends Controller {
+
+	private static String coreRestUri = Util.buildNemakiCoreUri() + "rest/";
+
+	private static Session getCmisSession(String repositoryId){
+		return CmisSessions.getCmisSession(repositoryId, session());
+	}
+
+	public static Result index(String repositoryId){
+	    	return list(repositoryId);
+	  }
+
+	public static Result list(String repositoryId){
+
+    	JsonNode result = Util.getJsonResponse(session(), getEndpoint(repositoryId) + "/list" );
+
+    	//TODO check status
+    	JsonNode configurations = result.get("result");
+
+    	List<model.Config> list = new ArrayList<model.Config>();
+
+    	if(configurations == null){
+    		configurations = Json.parse("[]");
+    	}else{
+    		Iterator<JsonNode>itr = configurations.elements();
+        	while(itr.hasNext()){
+        		JsonNode node = itr.next();
+
+        		model.Config config = new model.Config();
+        		config.key = node.get("key").asText();
+        		config.value = node.get("value").asText();
+        		config.isDefault = node.get("isDefault").asBoolean();
+
+        		list.add(config);
+        	}
+    	}
+
+
+    	//render
+    	if(Util.dataTypeIsHtml(request().acceptedTypes())){
+    		return ok(index.render(repositoryId, list));
+    	}else{
+    		return ok(configurations);
+
+    	}
+
+
+	}
+
+	private static String getEndpoint(String repositoryId){
+		return coreRestUri + "repo/" + repositoryId + "/config";
+	}
+
+}
