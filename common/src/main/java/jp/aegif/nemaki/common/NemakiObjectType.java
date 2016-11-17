651a67fd3059f35a8e7bd525de0efae40c75b89a
SUGIMOTO Takuma <takuma.sugimoto@aegif.jp>
2016-11-17 16:54:58 +0900

    Fix view
    Former-commit-id: bc61af2d25f5e6f70ab18077dc0dac7ad8929a32

--- common/src/main/java/jp/aegif/nemaki/common/NemakiObjectType.java
+++ common/src/main/java/jp/aegif/nemaki/common/NemakiObjectType.java
@@ -4,6 +4,6 @@ public class NemakiObjectType {
 	public static String nemakiUser = "nemaki:user";
 	public static String nemakiGroup = "nemaki:group";
 	public static String nemakiParentChildRelationship = "nemaki:parentChildRelationship";
-	public static String nemakiBidirectionalRelationship="nemaki:bidirectionalRelationship";
+	public static String nemakiBidirectionalRelationship ="nemaki:bidirectionalRelationship";
 
 }
