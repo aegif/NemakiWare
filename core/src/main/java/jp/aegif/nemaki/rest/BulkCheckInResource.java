8220bc21030415466e0be5858160475d1abc1b61
Ishii Akinori <akinori.ishii@aegif.jp>
2017-01-07 00:43:41 +0900

    assign endpoint

--- core/src/main/java/jp/aegif/nemaki/rest/BulkCheckInResource.java
+++ core/src/main/java/jp/aegif/nemaki/rest/BulkCheckInResource.java
@@ -2,6 +2,7 @@ package jp.aegif.nemaki.rest;
 
 import java.util.List;
 
+
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
@@ -11,6 +12,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -18,15 +20,25 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import jp.aegif.nemaki.businesslogic.ContentService;
+import jp.aegif.nemaki.cmis.service.VersioningService;
+import jp.aegif.nemaki.cmis.service.RelationshipService;
 
-@Path("/bulkCheckIn/{repositoryId}")
+@Path("/repo/{repositoryId}/bulkCheckIn")
 public class BulkCheckInResource extends ResourceBase {
 	private static final Log log = LogFactory.getLog(BulkCheckInResource.class);
 
 	private ContentService contentService;
+	private VersioningService versioningService;
+	private RelationshipService relationshipService;
 
 	public void setContentService(ContentService contentService) {
 		this.contentService = contentService;
+	}
+	public void setVersioningService(VersioningService versioningService) {
+		this.versioningService = versioningService;
+	}
+	public void setRelationshipService(RelationshipService relationshipService) {
+		this.relationshipService = relationshipService;
 	}
 
 
@@ -35,15 +47,18 @@ public class BulkCheckInResource extends ResourceBase {
 	@Path("/execute")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	public String execute( @PathParam("repositoryId") String repositoryId,
-			@FormParam("propertyId[]") List<String> propertyIds, @FormParam("propertyValue[]") List<String> propertyValues,
-			@FormParam("objectId[]") List<String> objectIds, @FormParam("changeToken[]") List<String> changeTokens,
-			@FormParam("comment") String comment, @FormParam("force") Boolean force,
-			@Context HttpServletRequest httpRequest) {
-		boolean status = true;
+
+	public String execute(MultivaluedMap<String,String> form){
 		JSONObject result = new JSONObject();
 		JSONArray errMsg = new JSONArray();
-
+		
+		String repositoryId = form.get("repositoryId").get(0);
+		String comment = form.get("comment").get(0);
+		Boolean force = form.get("force").get(0).equals("true");
+		List<String> propertyIds = form.get("propertyId");
+		List<String> propertyValues = form.get("propertyValue");
+		List<String> objectIds = form.get("objectId");
+		List<String> changeTokens = form.get("changeToken");
 
 		return result.toJSONString();
 	}
