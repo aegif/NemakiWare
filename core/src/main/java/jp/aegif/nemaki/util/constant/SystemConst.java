b3ba1ad675302e0060f774875fadb3f6234b8c23
SUGIMOTO Takuma <takuma.sugimoto@aegif.jp>
2017-05-09 12:13:40 +0900

    DateFormat fix again

--- core/src/main/java/jp/aegif/nemaki/util/constant/SystemConst.java
+++ core/src/main/java/jp/aegif/nemaki/util/constant/SystemConst.java
@@ -3,5 +3,5 @@ package jp.aegif.nemaki.util.constant;
 public interface SystemConst {
 	public static final String NEMAKI_CONF_DB = "nemaki_conf";
 
-	public static final String DATETIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ssXXX";
+	public static final String DATETIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
 }
