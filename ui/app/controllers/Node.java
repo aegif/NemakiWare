682594e6a3c7e62bd20c60c8d25e05b82cf063b2
Ishii Akinori <akinori.ishii@aegif.jp>
2017-06-17 03:58:14 +0900

    missed some changes

--- ui/app/controllers/Node.java
+++ ui/app/controllers/Node.java
@@ -122,13 +122,13 @@ public class Node extends Controller {
 	public Result index(String repositoryId) {
 		Session session = getCmisSession(repositoryId);
 		Folder root = session.getRootFolder();
-		return showChildren(repositoryId, root.getId(), 0, "cmis:name DESC",null);
+		return showChildren(repositoryId, root.getId(), 0, "cmis:name ASC",null);
 	}
 	@Secure
 	public Result index(String repositoryId, int currentPage) {
 		Session session = getCmisSession(repositoryId);
 		Folder root = session.getRootFolder();
-		return showChildren(repositoryId, root.getId(), currentPage, "cmis:name DESC",null);
+		return showChildren(repositoryId, root.getId(), currentPage, "cmis:name ASC",null);
 	}
 	@Secure
 	public Result index(String repositoryId, int currentPage, String orderBy) {
@@ -166,11 +166,11 @@ public class Node extends Controller {
 	}
 	@Secure
 	public Result showChildren(String repositoryId, String objectId){
-		return showChildren(repositoryId,  objectId, 0,"cmis:name DESC",null);
+		return showChildren(repositoryId,  objectId, 0,"cmis:name ASC",null);
 	}
 	@Secure
 	public Result showChildren(String repositoryId, String objectId, int currentPage){
-		return showChildren(repositoryId,  objectId, currentPage,"cmis:name DESC",null);
+		return showChildren(repositoryId,  objectId, currentPage,"cmis:name ASC",null);
 	}
 
 	@Secure
