df29e82dbff1c0dbdfa7599c5301c8d6bc33d94f
rileyduke <rileyduke@gmail.com>
2020-03-18 16:02:50 +0900

    docker-run folder

--- setup/docker_new/docker-build/nemaki-setup-ui/setup.sh
+++ setup/docker_new/docker-build/nemaki-setup-ui/setup.sh
@@ -1,8 +1,5 @@
-#mvn -f /home/app/action/pom.xml install
-#mvn -f /home/app/common/pom.xml install
-#mvn -f /home/app/core/pom.xml install
-#mvn -f /home/app/solr/pom.xml install
-# cd /home/app && mvn package
-# cd /home/app && mvn install
-cd /home/app/ui/ && ./activator update
+#!/bin/bash
+java -version
+export ACTIVATOR_HOME=/home/app/ui
+cd /home/app/ui/ && ./activator eclipse
 cd /home/app/ui/ && ./activator war
